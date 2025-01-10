24fc7ef621fcc7358b67dc1b46e5f7a0670ff743
Puja Saikia <pujasaikia2k4@gmail.com>
2025-01-11 00:14:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); 
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); 
+
+  document.getElementById("login-section").style.display = "none";
+  document.querySelector(".chat-container").style.display = "block";
+}
+
+function onFailure(error) {
+  alert("Login Failed. Please try again.");
+  console.error("Error details:", error);
+}
+
+
+
