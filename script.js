f85bdaa415b071ad2ca6b9e005c9591844dde5f1
Puja Saikia <pujasaikia2k4@gmail.com>
2025-01-10 23:55:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
- function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); 
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); 
-  
+function onSignIn(response) {
+  const userCredential = jwt_decode(response.credential);
+  console.log('ID:', userCredential.sub);
+  console.log('Name:', userCredential.name);
+  console.log('Image URL:', userCredential.picture);
+  console.log('Email:', userCredential.email);
+
   document.getElementById("login-section").style.display = "none";
   document.querySelector(".chat-container").style.display = "block";
 }
+
 function onFailure(error) {
   alert("Login Failed. Please try again.");
   console.error("Error details:", error);
